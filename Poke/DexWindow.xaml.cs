c76e42e24ce35a87599b3995fdab15ca0b7f64f6
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-02-05 21:51:52 +0530

    Dex page slide

--- Poke/DexWindow.xaml.cs
+++ Poke/DexWindow.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace Poke
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+namespace Poke
 {
     public partial class DexWindow
     {
@@ -6,5 +12,37 @@
         {
             InitializeComponent();
         }
+
+        void Frame_OnNavigating(object Sender, NavigatingCancelEventArgs E)
+        {
+            if (E.Content is Page page)
+            {
+                var transform = new TranslateTransform();
+
+                page.RenderTransform = transform;
+
+                var anim = new DoubleAnimation
+                {
+                    Duration = TimeSpan.FromSeconds(0.3),
+                    DecelerationRatio = 0.7
+                };
+
+                switch (E.NavigationMode)
+                {
+                    case NavigationMode.New:
+                        anim.From = 500;
+                        anim.To = 0;
+                        break;
+
+                    case NavigationMode.Back:
+                        anim.To = 0;
+                        anim.From = -500;
+                        break;
+                }
+
+                transform.BeginAnimation(TranslateTransform.XProperty, anim);
+            }
+        }
+
     }
 }
