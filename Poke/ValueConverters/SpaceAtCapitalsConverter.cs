53edd2c923c801ea5668e7017a950032fcf4d942
Mathew Sachin <mathew.sachin.git@outlook.com>
2018-02-05 22:50:54 +0530

    Space at Capitals in Natures page

---
+++ Poke/ValueConverters/SpaceAtCapitalsConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Poke
+{
+    public class SpaceAtCaptitalsConverter : IValueConverter
+    {
+        public object Convert(object Value, Type TargetType, object Parameter, CultureInfo Culture)
+        {
+            return Value == null ? Parameter : Value.ToString().SpaceAtCapitals();
+        }
+
+        public object ConvertBack(object Value, Type TargetType, object Parameter, CultureInfo Culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+}
